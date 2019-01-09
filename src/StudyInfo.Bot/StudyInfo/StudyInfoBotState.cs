968cc3e76c71e6b7faa4f622af9a17d4a78eb535
Van Acoleyen, Esli <vanacoleyene@delawareconsulting.com>
2019-01-09 09:57:34 +0100

    remove unused files

--- src/StudyInfo.Bot/StudyInfo/StudyInfoBotState.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
-
-namespace StudyInfo.Bot.StudyInfo
-{
-    public class StudyInfoBotState : DialogState
-    {
-        public string[] CourseEntities { get; set; }
-    }
-}
